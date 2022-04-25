2cba0c38ccc1cf6540e42737935723fd1c5b6d2e
Emily Landers <emily.j.landers@gmail.com>
2022-04-25 13:30:20 -0400

    cc05

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -13,6 +13,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
         
+    def __str__(self):
+        # returns a collection of all values within linked list
+        string = ""
+        current = self.head
+        while current:
+            string += f"{{ {current.value} }} -> "
+            current = current.next
+        return string + "NULL"
+        
     def insert(self, value):
         #creating a new node with the correct value
        self.head = Node(value, self.head)
