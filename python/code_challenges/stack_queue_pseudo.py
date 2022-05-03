18cf4e6372b8ee3d0268aa6ed350a63327c4d2c0
Emily Landers <emily.j.landers@gmail.com>
2022-05-02 23:52:57 -0400

    adds code

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,23 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+        
+    def enqueue(self, value):
+        while len(self.stack1) != 0:
+            self.stack1.pop()
+            self.stack1.append(value)
+        while len(self.stack2) != 0:
+            self.stack1.append(self.stack2[-1])
+            self.stack2.pop()
+            
+    def dequeue(self):
+        if len(self.stack1) == 0:
+            print("is empty")
+        value = self.stack1[-1]
+        self.stack1.pop()
+        return value
+q = PseudoQueue()
+print(q.stack1)        
