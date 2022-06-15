328cf27b2cde3bf5ec47ac7dc6099069141a5289
Emily Landers <emily.j.landers@gmail.com>
2022-06-15 11:49:25 -0400

    cc31

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,22 @@
 from data_structures.hashtable import Hashtable
 
 
-def first_repeated_word():
-    pass
+def first_repeated_word(txt):
+
+    word_assesser = ""
+    hash = Hashtable()
+
+    for char in txt:
+        char_lower = char.lower()
+        if ord(char_lower) in range(ord("a"), ord("z")+1):
+            word_assesser += char_lower
+        elif len(word_assesser):
+                if hash.contains(word_assesser):
+                    return word_assesser
+                else:
+                    hash.set(word_assesser, None)
+                    word_assesser = ""
+
+    if len(word_assesser) and hash.contains(word_assesser):
+        return word_assesser
+    return None
--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,9 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree1, tree2):
+    first = BinaryTree.pre_order(tree1)
+    second = BinaryTree.pre_order(tree2)
+    x = set(first)
+    y = set(second)
+    return x.intersection(y)
