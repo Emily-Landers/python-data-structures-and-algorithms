36a1c34b52a38336c2f0509ea72863c0244f8825
Emily Landers <emily.j.landers@gmail.com>
2022-06-15 12:04:24 -0400

    cc 33

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,9 @@
-from data_structures.hashtable import Hashtable
+def left_join(table1, table2):
 
-
-def left_join():
-    pass
+    joined_table = {}
+    for key, value in table1.items():
+        if table2.get(key):
+            joined_table[key] = [value, table2.get(key)]
+        else:
+            joined_table[key] = [value, None]
+    return joined_table
