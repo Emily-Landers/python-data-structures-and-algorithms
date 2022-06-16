9a3a44356d5f4ea526775acaabd9d3914cd9deae
Emily Landers <emily.j.landers@gmail.com>
2022-06-16 12:33:42 -0400

    cc 38

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -63,3 +63,18 @@ class Vertex:
 
     def __init__(self, value):
         self.value = value
+        
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
---
+++ python/docs/graph_depth_first/README.md
@@ -0,0 +1,11 @@
+# Graphs
+
+- Graphs are a data structure similar to binary tree, but with some structural differences.
+
+## Challenge
+
+- Implement a depth first traversal on your graph
+
+## Approach & Efficiency
+
+- used similar approach to tree
